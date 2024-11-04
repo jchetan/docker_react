3b62f10e118d75349a1c718847a251655247ee68
Chetan Janardhana <janachet@amazon.com>
2024-11-04 16:34:41 +0000

    fetching data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,21 @@
+import { useState, useEffect } from 'react'
+
 function App() {
+  
+  useEffect(
+    () => {
+      const fetchApi = async () => {
+        const response = await fetch('/api');
+        const json = await response.json();
+        if (response.ok) {
+          console.log(json);
+        }
+      }
+      fetchApi();
+    },
+    []
+  );
+
   return (
     <div>
       Hello World
